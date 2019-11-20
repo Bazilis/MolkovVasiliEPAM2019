0ce1b007de7057183eb55395b3b1ba0aefef97ad
Василий Василий <vas666@yandex.by>
2019-11-20 15:32:58 +0300

    Add properties Add constructor Add overloaded methods

--- Task2.1_NET/ClassLibrary/Vector.cs
+++ Task2.1_NET/ClassLibrary/Vector.cs
@@ -4,6 +4,30 @@ namespace ClassLibrary
 {
     public class Vector
     {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
 
+        public Vector(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public static Vector operator +(Vector first, Vector second)
+        {
+            return new Vector((first.X + second.X), (first.Y + second.Y), (first.Z + second.Z));
+        }
+
+        public static Vector operator -(Vector first, Vector second)
+        {
+            return new Vector((first.X - second.X), (first.Y - second.Y), (first.Z - second.Z));
+        }
+
+        public static double operator *(Vector first, Vector second)
+        {
+            return ((first.X * second.X) + (first.Y * second.Y) + (first.Z * second.Z));
+        }
     }
 }
