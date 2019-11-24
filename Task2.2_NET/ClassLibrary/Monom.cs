f07de3798ae93d384835477a861cc1e9cc649ee6
Василий Василий <vas666@yandex.by>
2019-11-24 03:27:24 +0300

    Create class Monom Add overloaded operators

---
+++ Task2.2_NET/ClassLibrary/Monom.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Monom
+    {
+        public double Coefficient { get; set; }
+
+        public double Power { get; set; }
+
+        public Monom(double coefficient, double power)
+        {
+            Coefficient = coefficient;
+            Power = power;
+        }
+
+        public Monom()
+        {
+
+        }
+
+        public static Monom operator *(Monom first, Monom second)
+        {
+            return new Monom(first.Coefficient * second.Coefficient, first.Power + second.Power);
+        }
+
+        public static Monom operator /(Monom first, Monom second)
+        {
+            return new Monom(first.Coefficient / second.Coefficient, first.Power - second.Power);
+        }
+
+        public static Monom operator *(Monom monom, double number)
+        {
+            return new Monom(monom.Coefficient * number, monom.Power);
+        }
+
+        public static Monom operator /(Monom monom, double number)
+        {
+            return new Monom(monom.Coefficient / number, monom.Power);
+        }
+    }
+}
