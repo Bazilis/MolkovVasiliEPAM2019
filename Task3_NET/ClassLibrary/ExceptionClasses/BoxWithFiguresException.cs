8077d70000e4a503bd6d41ead4b2486a6e85af2a
Василий Василий <vas666@yandex.by>
2019-12-14 21:33:52 +0300

    Add BoxWithFigures class Add BoxWithFiguresException class

---
+++ Task3_NET/ClassLibrary/ExceptionClasses/BoxWithFiguresException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.ExceptionClasses
+{
+    public class BoxWithFiguresException : Exception
+    {
+        public BoxWithFiguresException(string message) : base(message)
+        {
+
+        }
+
+        public BoxWithFiguresException() : this("Box with figures exception")
+        {
+
+        }
+    }
+}
