dbc8417e16260cc01a15e3981f4136192ec881c6
Василий Василий <vas666@yandex.by>
2019-12-22 23:53:59 +0300

    Create class library Add Client class Add Server class

---
+++ Task4_NET/ClassLibrary/Client.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Client
+    {
+        public delegate void MessageFromServer(string message);
+        public event MessageFromServer OnMessageFromServer;
+
+        public string ClientName { get; private set; }
+
+        public int Port { get; private set; }
+
+        public string HostName { get; private set; }
+
+        private TcpClient ClientTCP { get; set; }
+
+        private NetworkStream Stream { get; set; }
+
+        public Client(string clientName)
+        {
+            ClientName = clientName;
+            Port = 8888;
+            HostName = "127.0.0.1";
+            ClientTCP = new TcpClient();
+        }
+
+        public Client(string clientName, int port, string hostName)
+        {
+            ClientName = clientName;
+            Port = port;
+            HostName = hostName;
+            ClientTCP = new TcpClient();
+        }
+
+        public void Connect()
+        {
+            ClientTCP.Connect(IPAddress.Parse(HostName), Port);
+
+            Stream = ClientTCP.GetStream();
+        }
+
+        public void SendMessageToServer(string message)
+        {
+            string sendString = ClientName + ">" + message;
+
+            byte[] sendBytes = Encoding.Unicode.GetBytes(sendString);
+
+            Stream.Write(sendBytes, 0, sendBytes.Length);
+        }
+
+        public void ReceiveMessageFromServer(NetworkStream stream)
+        {
+            byte[] receivedBytes = new byte[1024];
+
+            int bytesCount = stream.Read(receivedBytes, 0, receivedBytes.Length);
+
+            string receivedString = Encoding.Unicode.GetString(receivedBytes, 0, bytesCount);
+
+            OnMessageFromServer?.Invoke(receivedString);
+        }
+    }
+}
---
+++ Task4_NET/ClassLibrary/Server.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Server
+    {
+        public delegate void MessageFromClient(string message);
+        public event MessageFromClient OnMessageFromClient;
+
+        private static readonly TcpListener ServerTCP;
+
+        public int Port { get; private set; }
+
+        public string HostName { get; private set; }
+
+        public void StartServer()
+        {
+            ServerTCP.Start();
+
+            while (true)
+            {
+                TcpClient client = ServerTCP.AcceptTcpClient();
+
+                NetworkStream stream = client.GetStream();
+
+                new Thread(delegate ()
+                {
+                    try
+                    {
+                        ReceiveMessageFromClient(stream);
+                        SendMessageToClient(stream);
+                        stream.Close();
+                        client.Close();
+                    }
+                    catch (Exception exception)
+                    {
+                        stream.Close();
+                        client.Close();
+                        throw new Exception(exception.ToString());
+                    }
+                }).Start();
+            }
+        }
+
+        public void ReceiveMessageFromClient(NetworkStream stream)
+        {
+            byte[] receivedBytes = new byte[1024];
+
+            int bytesCount = stream.Read(receivedBytes, 0, receivedBytes.Length);
+
+            string receivedString = Encoding.Unicode.GetString(receivedBytes, 0, bytesCount);
+
+            OnMessageFromClient?.Invoke(receivedString);
+        }
+
+        public void SendMessageToClient(NetworkStream stream)
+        {
+            string sendString = "Your message has been successfully delivered";
+
+            byte[] sendBytes = Encoding.Unicode.GetBytes(sendString);
+
+            stream.Write(sendBytes, 0, sendBytes.Length);
+        }
+    }
+}
