97179b338e9f2574e67e4079b1bc06cc42828737
Василий Василий <vas666@yandex.by>
2019-12-28 03:00:26 +0300

    Create class library Add Node class Add StudentsTest class

---
+++ Task5_NET/ClassLibrary/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Node<T> where T : IComparable
+    {
+        public Node<T> ParentNode { get; set; }
+
+        public Node<T> LeftNode { get; set; }
+
+        public Node<T> RightNode { get; set; }
+
+        public T NodeData { get; set; }
+
+        public Node(T nodeData)
+        {
+            NodeData = nodeData;
+        }
+
+        public override string ToString()
+        {
+            return $"NodeData-> {NodeData}";
+        }
+    }
+}
---
+++ Task5_NET/ClassLibrary/StudentsTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class StudentsTest : IComparable<StudentsTest>
+    {
+        private string nameOfStudent;
+
+        private int gradeOfStudent;
+
+        private string topicOfTest;
+
+        public string NameOfStudent
+        {
+            get
+            {
+                return nameOfStudent;
+            }
+            set
+            {
+                if (nameOfStudent.Length < 1)
+                    throw new ArgumentException("Name of student is empty");
+
+                else
+                    nameOfStudent = value;
+            }
+        }
+
+        public int GradeOfStudent
+        {
+            get
+            {
+                return gradeOfStudent;
+            }
+            set
+            {
+                if (gradeOfStudent < 1 || gradeOfStudent > 10)
+                    throw new ArgumentException("Grade of student is out of range. It should be from 1 to 10 points");
+
+                else
+                    gradeOfStudent = value;
+            }
+        }
+
+        public string TopicOfTest
+        {
+            get
+            {
+                return topicOfTest;
+            }
+            set
+            {
+                if (topicOfTest.Length < 1)
+                    throw new ArgumentException("Topic of test is empty");
+
+                else
+                    topicOfTest = value;
+            }
+        }
+
+        public DateTime DateOfTest { get; private set; }
+
+        public StudentsTest(string nameOfStudent, int markOfStudent, string topicOfTest, DateTime dateOfTest)
+        {
+            NameOfStudent = nameOfStudent;
+            GradeOfStudent = markOfStudent;
+            TopicOfTest = topicOfTest;
+            DateOfTest = dateOfTest;
+        }
+
+        public int CompareTo(StudentsTest student)
+        {
+            if (student != null)
+                return GradeOfStudent.CompareTo(student.GradeOfStudent);
+            else
+                throw new ArgumentException("Impossible to compare");
+        }
+
+        public override string ToString()
+        {
+            return $"Name of student-> {NameOfStudent}; grade of student-> {GradeOfStudent}; topic of test-> {TopicOfTest}; date of test-> {DateOfTest}";
+        }
+    }
+}
